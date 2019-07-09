07e244fbe057a77968936d5159cc3100e77fdfae
amartinez2 <austinamartinez2@gmail.com>
2019-07-08 23:59:50 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Guy from './Guy.js';
+import TurtleDB from 'turtledb';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: null
+    }
+    const mydb = new TurtleDB('example');
+    // Link a remote tortoiseDB database to sync to
+    mydb.setRemote('http://127.0.0.1:4000');
+
+    // CRUD Operations - all return promises
+    mydb.create({ _id: 'firstTurtle', species: 'Sea Turtle' });
+    let turt = mydb.read('firstTurtle');
+    let ace = turt.then(result => {
+      this.setState({name : result.name});
+    });
+
+    // Sync
+    mydb.sync();
+  }
+
+  render() {
+    console.log("waffles");
+    console.log(this.state.name);
+    return (
+      <div className="App">
+        <h1>{this.state.name}</h1>
+        <Guy />
+      </div>
+    )
+  }
 }
 
 export default App;
